edc07203e96ad537359ce182947d4be6821c5581
henry <henry@incuna.com>
2013-01-27 16:42:52 +0000

    Add script to fetch Presentation Gists. Uses base64.js to decode.

---
+++ get-presentations.js
@@ -0,0 +1,58 @@
+// For readability, 'presentation' will be replaced by 'talk'.
+// 
+// This script parses .gitmodules for Gist IDs, then fetches each Gist's files.
+$(function() {
+    var gitModulesUrl = 'https://api.github.com/repos/henrahmagix/presentations/contents/.gitmodules/';
+    var gistAPI = 'https://api.github.com/gists/';
+
+    function getTalks(callback) {
+        var talks;
+        $.get(gitModulesUrl, function(data, textStatus, jqXHR) {
+            talks = getGistsFromGitModules(Base64.decode(data.content));
+            callback(talks);
+        });
+    }
+
+    function getGistsFromGitModules(str) {
+        var submodules = [];
+        var getSegments = /\[submodule.*\n[^\[]*\n[^\[]*/g;
+        var segments = str.match(getSegments);
+        var segment;
+        var gistId;
+        for (var i = 0; i < segments.length; i++) {
+            segment = segments[i];
+            gistId = getGistId(segment);
+            if (gistId) {
+                submodules.push({
+                    id: gistId,
+                    name: getGistName(segment)
+                });
+            }
+        }
+        return submodules;
+    }
+
+    function getGistId(str) {
+        var url = /gist\.github\.com:([0-9]+)/;
+        var matches = str.match(url);
+        return (matches) ? matches[1] : matches;
+    }
+
+    function getGistName(str) {
+        var path = /path = (.+)/;
+        return str.match(path)[1];
+    }
+
+    function showTalks(talks) {
+        var area = $('.presentations');
+        var talk;
+        for (var i = 0; i < talks.length; i++) {
+            talk = talks[i];
+            $.get(gistAPI + talk.id, function(data, textStatus, jqXHR) {
+                area.append('<div class="presentation"><h3><a href="#' + talk.id + '">' + talk.name + '</a></h3></div>');
+            });
+        }
+    }
+
+    getTalks(showTalks);
+});
