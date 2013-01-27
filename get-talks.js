7a91e1ad1270cf3b8760ff88c87474c8fe0b25a8
henry <henry@incuna.com>
2013-01-27 21:58:10 +0000

    Show talks inline, rendered with showdown.js.
    Remove cruft from index.css. Style markdown-content as brown-box
    like previous paragraph styling. Correctly point to moved lib
    scripts.

--- get-talks.js
+++ get-talks.js
@@ -2,6 +2,7 @@
 $(function() {
     var gitModulesUrl = 'https://api.github.com/repos/henrahmagix/presentations/contents/.gitmodules/';
     var gistAPI = 'https://api.github.com/gists/';
+    var markdownAPI = 'https://api.github.com/markdown';
 
     function getTalks(callback) {
         var talks;
@@ -12,7 +13,7 @@ $(function() {
     }
 
     function getGistsFromGitModules(str) {
-        var submodules = [];
+        var submodules = {};
         var getSegments = /\[submodule.*\n[^\[]*\n[^\[]*/g;
         var segments = str.match(getSegments);
         var segment;
@@ -21,10 +22,10 @@ $(function() {
             segment = segments[i];
             gistId = getGistId(segment);
             if (gistId) {
-                submodules.push({
+                submodules[gistId] = {
                     id: gistId,
                     name: getGistName(segment)
-                });
+                };
             }
         }
         return submodules;
@@ -41,16 +42,71 @@ $(function() {
         return str.match(path)[1];
     }
 
-    function showTalks(talks) {
+    function insertTalks(talks) {
         var area = $('.talks');
-        var talk;
-        for (var i = 0; i < talks.length; i++) {
-            talk = talks[i];
-            $.get(gistAPI + talk.id, function(data, textStatus, jqXHR) {
-                area.append('<div class="talk"><h3><a href="#' + talk.id + '">' + talk.name + '</a></h3></div>');
+        var wrapper = $('<article class="talk"><h3><a class="gist"></a></h3><div class="markdown-content"></div></article>');
+        for (var id in talks) {
+            $.get(gistAPI + id, function(data, textStatus, jqXHR) {
+                var $talk = wrapper.clone();
+                var thisTalk;
+                var markdown;
+                talks[id].description = data.description;
+                talks[id].files = data.files;
+                thisTalk = talks[id];
+                if (thisTalk.hasOwnProperty('files')) {
+                    for (file in thisTalk.files) {
+                        file = thisTalk.files[file];
+                        if (file.language === 'Markdown') {
+                            markdown = renderTalk(file.content);
+                            $talk.find('.markdown-content').hide().append(markdown);
+                            $talk.find('.gist')
+                                .attr({
+                                    'data-gist-id': id,
+                                    'data-talk': 'closed',
+                                    'href': '//gist.github.com/' + id
+                                })
+                                .text(thisTalk.description);
+                            area.append($talk);
+                        }
+                    }
+                }
             });
         }
+        // Click events.
+        $(document).on('click', '.gist', showTalk);
     }
 
-    getTalks(showTalks);
+    function showTalk(e) {
+        // Stop regular click event.
+        e.preventDefault();
+        var $talkLink = $(e.currentTarget);
+        var $talkContent = $talkLink.closest('.talk').find('.markdown-content');
+        var gistId = $talkLink.attr('data-gist-id');
+        if (gistId) {
+            if ($talkLink.attr('data-talk') === 'open') {
+                $talkLink.attr('data-talk', null);
+                $talkContent.fadeOut(function() {
+                    $('.title, .links').slideDown().promise().done(function() {
+                        $talkLink.attr('data-talk', 'closed');
+                    });
+                });
+            } else if ($talkLink.attr('data-talk') === 'closed') {
+                $talkLink.attr('data-talk', null);
+                $('.title, .links').slideUp().promise().done(function() {
+                    $talkContent.fadeIn(function() {
+                        $talkLink.attr('data-talk', 'open');
+                    });
+                });
+            }
+        }
+        return false;
+    };
+
+    function renderTalk(str) {
+        var converter = new Showdown.converter();
+        return converter.makeHtml(str);
+    }
+
+    getTalks(insertTalks);
+
 });
