1f60fca85df367afb07c86b8a38a7e35967f6c84
Henry Blyth <henry@henryblyth.com>
2016-02-06 18:47:25 +0000

    Commit previous site versions as jekyll builds.
    For each version tag, I moved to it with `git checkout`, built it with `jekyll build`, then renamed the built `_site` folder to the version tag. Then for every subsequent version, the previous versions needed to be removed as they were copied into the `_site` folder.
    
    After all versions were done, `git checkout master` and they're all untracked files, so this commit adds them all.

---
+++ v2/text-expand.js
@@ -0,0 +1,170 @@
+$(function() {
+    var text = $('.title');
+    var textPos = text.position();
+    var textColor = text.css('color');
+    var numberOfShadows = 30;
+    text.css({
+        'color': '#edede7',
+        'cursor': 'default',
+        position: 'relative',
+        'z-index': numberOfShadows + 1
+    });
+    var textShadow = text.clone().css({
+        position: 'absolute',
+        top: textPos.top,
+        left: textPos.left,
+        'z-index': numberOfShadows,
+        width: '100%',
+        'transform-origin': '49% bottom',
+        '-o-transform-origin': '49% bottom',
+        '-ms-transform-origin': '49% bottom',
+        '-moz-transform-origin': '49% bottom',
+        '-webkit-transform-origin': '49% bottom'
+    });
+    var texts = [];
+    var scaleDecrement;
+    var translateDecrement;
+    var bgColorHex = colorToHex($('body').css('background-color'));
+    var bgColorRgb = getRgbArray($('body').css('background-color'));
+    var textColorRgb = getRgbArray(textColor);
+    var colorStep = bgColorRgb.map(function(val, i) {
+        return (parseInt(val, 10) - parseInt(textColorRgb[i], 10)) / numberOfShadows;
+    });
+    for (var i = 0, clone, color, transform; i < numberOfShadows; i++) {
+        clone = textShadow.clone();
+        scaleDecrement = 1 - (0.01 * i);
+        translateDecrement = i * 2;
+        color = colorToHex(textColor, colorStep.map(function(val, j) {
+            return val * (i + 1);
+        }));
+        transform = [
+            'scale3d(' + [scaleDecrement, scaleDecrement, 1].join(', ') + ')',
+            'translate3d(' + [0, translateDecrement + '%', 0].join(', ') + ')'
+        ];
+        clone.css({
+            'color': '#' + color,
+            'z-index': numberOfShadows - i,
+            'transform': transform.join(' '),
+            '-o-transform': transform.join(' '),
+            '-ms-transform': transform.join(' '),
+            '-moz-transform': transform.join(' '),
+            '-webkit-transform': transform.join(' ')
+        });
+        texts.push(clone.get(0));
+    }
+    texts = $(texts);
+    texts.hide();
+    text.after(texts);
+
+    var shadowIndex = 0;
+    var shadowAction;
+    var hoverAnim;
+    function hoverShadow() {
+        if (shadowIndex < numberOfShadows && shadowIndex >= 0) {
+            animateShadow(shadowAction);
+            requestAnimationFrame(hoverShadow, text);
+            return true;
+        } else {
+            cancelAnimationFrame(hoverAnim);
+            return false;
+        }
+    }
+    function animateShadow(action) {
+        var shadow = texts.eq(shadowIndex);
+        if (action === 'show' && shadowIndex < numberOfShadows - 1) {
+            shadow.show();
+            shadowIndex++;
+            return true;
+        } else if (action === 'hide' && shadowIndex > 0) {
+            shadow.hide();
+            shadowIndex--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    var animateOnLoad = setInterval(function() {
+        if (! hoverAnim) {
+            animateShadow('show');
+        } else {
+            clearInterval(animateOnLoad);
+        }
+    }, 20);
+    text.hover(function(e) {
+        // Mouse in
+        cancelAnimationFrame(hoverAnim);
+        shadowAction = 'hide';
+        hoverAnim = requestAnimationFrame(hoverShadow, text);
+    }, function(e) {
+        // Mouse out
+        cancelAnimationFrame(hoverAnim);
+        shadowAction = 'show';
+        hoverAnim = requestAnimationFrame(hoverShadow, text);
+    });
+
+    function colorToHex(color, addAmount) {
+        var rgb;
+        if (color.substr(0, 1) === '#') {
+            // This is already a hex string, so chop of the hash.
+            rgb = color.substr(1);
+            // If this is a 3-character hex string, make it 6-characters long.
+            if (rgb.length === 3) {
+                // Get an array with each value duplicated, then join it into a
+                // string.
+                rgb = $.map(rgb.split(''), function(val) {
+                    return val + val;
+                }).join('');
+            }
+        } else {
+            var digits = /(.*?)rgb\((\d+), ?(\d+), ?(\d+)\)/.exec(color);
+            var red = parseInt(digits[2]);
+            var green = parseInt(digits[3]);
+            var blue = parseInt(digits[4]);
+            // Adjust values if addAmount is passed.
+            if (typeof addAmount !== 'undefined') {
+                if (typeof addAmount === 'number') {
+                    red += addAmount;
+                    green += addAmount;
+                    blue += addAmount;
+                } else if (addAmount instanceof Array) {
+                    red += addAmount[0] || 0;
+                    green += addAmount[1] || 0;
+                    blue += addAmount[2] || 0;
+                } else if (typeof addAmount !== 'string') {
+                    if (addAmount.hasOwnProperty('red')) {
+                        red += addAmount.red;
+                    } else if (addAmount.hasOwnProperty('green')) {
+                        green += addAmount.green;
+                    } else if (addAmount.hasOwnProperty('blue')) {
+                        blue += addAmount.blue;
+                    }
+                }
+            }
+            // Get a base10 number.
+            rgb = blue | (green << 8) | (red << 16);
+            rgb = rgb.toString(16);
+        }
+        // Prepend rgb with zeros until it is 6 characters long.
+        while (rgb.length < 6) {
+            rgb = '0' + rgb;
+        }
+        return rgb;
+    }
+
+    function getRgbArray(color) {
+        var hex;
+        var rgb = [];
+        if (color.substr(0, 1) === '#') {
+            hex = color.substr(1);
+            for (var i = 0; i < 3; i++) {
+                rgb[i] = parseInt(hex[i * 2] + hex[i * 2 + 1], 16);
+            }
+        } else {
+            hex = color.match(/rgb\(([^)]+)/);
+            if (hex !== null && hex[1]) {
+                rgb = hex[1].split(/, ?/);
+            }
+        }
+        return rgb;
+    }
+});
---
+++ v3-blue/config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "_sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :compressed
+
+add_import_path "font-awesome-4.3.0/scss"
