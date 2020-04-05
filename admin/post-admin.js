57b7a0c11842041ab018a944e63dbffad750482f
Henry Blyth <blyth.henry@gmail.com>
2020-04-05 16:59:30 +0100

    smol improvements

--- admin/post-admin.js
+++ admin/post-admin.js
@@ -2,14 +2,16 @@ import { Admin } from '/admin/admin.js';
 import { EditPost } from '/admin/post-edit.js';
 import { createHTML } from '/admin/utils.js';
 
+const contentElement = document.getElementById('content');
+
 const buildWaiting = createHTML('<p id="build-waiting">Build waiting <button class="button-link" onclick="location.reload()"><i class="fas fa-sync-alt"></i></button></p>');
-document.getElementById('content').before(buildWaiting);
+contentElement.before(buildWaiting);
 
 const admin = new Admin({
   handleLogin(loggedIn) {
     if (loggedIn) {
-      const edit = new EditPost(document.getElementById('content'));
-      edit.insertBefore(content);
+      const edit = new EditPost(contentElement);
+      edit.insertBefore(contentElement);
 
       admin.api.fetch('/pages/builds').then(res => {
         if (window.github_data.build_revision === res[0].commit) {
