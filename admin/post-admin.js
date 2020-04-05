d4d8e9b3fb2c53f2b0fffd83d6c81b8bea701c35
Henry Blyth <blyth.henry@gmail.com>
2020-04-05 16:33:01 +0100

    show if the page commit does not match last build

--- admin/post-admin.js
+++ admin/post-admin.js
@@ -1,12 +1,21 @@
 import { Admin } from '/admin/admin.js';
 import { EditPost } from '/admin/post-edit.js';
+import { createHTML } from '/admin/utils.js';
 
-const edit = new EditPost(document.getElementById('content'));
+const buildWaiting = createHTML('<p id="build-waiting">Build waiting <button class="button-link" onclick="location.reload()"><i class="fas fa-sync-alt"></i></button></p>');
+document.getElementById('content').before(buildWaiting);
 
-new Admin({
+const admin = new Admin({
   handleLogin(loggedIn) {
     if (loggedIn) {
+      const edit = new EditPost(document.getElementById('content'));
       edit.insertBefore(content);
+
+      admin.api.fetch('/pages/builds').then(res => {
+        if (window.github_data.build_revision === res[0].commit) {
+          buildWaiting.hidden = true;
+        }
+      });
     }
   },
 });
