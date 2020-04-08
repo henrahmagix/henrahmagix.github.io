63c0fb59fbd8e2b5112e4f1afb18b8ef83d6e166
Henry Blyth <blyth.henry@gmail.com>
2020-04-08 12:12:35 +0100

    remove latest change: use view history at bottom

--- admin/page-builds.js
+++ admin/page-builds.js
@@ -3,9 +3,6 @@ import { createHTML, show } from '/admin/utils.js';
 
 export class PageBuildStatus {
   get waitingEl() { return this.el.querySelector('.waiting'); }
-  get historyLink() { return this.el.querySelector('.history'); }
-  get latestDiffButton() { return this.el.querySelector('.show-latest-diff'); }
-  get latestDiffEl() { return this.el.querySelector('.latest-diff'); }
 
   constructor() {
     this.api = new Api();
@@ -18,48 +15,16 @@ export class PageBuildStatus {
             <i class="fas fa-sync-alt"></i>
           </button>
         </p>
-        <p>
-          <button hidden class="button-link show-latest-diff"></button>
-          <pre hidden class="latest-diff"></pre>
-        </p>
       </div>
     `);
-
-    this.latestDiffButton.addEventListener('click', () => this.toggleDiff())
   }
 
   async checkForCommit(commit) {
     this.commit = commit;
 
-    this.latestDiffButton.innerText = `Show last change: ${this.commit.slice(0, 7)}`;
-    show(this.latestDiffButton);
-
     this.api.makeRequest('/pages/builds').then(res => {
       const latest = res[0];
       show(this.waitingEl, latest.status !== 'built' || this.commit !== latest.commit);
-    });
-  }
-
-  async toggleDiff() {
-    this.showingDiff = !this.showingDiff;
-    show(this.latestDiffEl, this.showingDiff);
-
-    if (this.showingDiff) {
-      this.latestDiffEl.innerText = await this.getDiff();
-    }
-  }
-
-  async getDiff() {
-    this.diffCache = this.diffCache || {};
-    if (this.diffCache[this.commit]) {
-      return this.diffCache[this.commit];
-    }
-
-    const headers = new Headers();
-    headers.set('Accept', 'application/vnd.github.v3.diff');
-
-    return this.diffCache[this.commit] = await this.api.makeRequest(`/commits/${this.commit}`, {
-      headers,
     });
   }
 }
