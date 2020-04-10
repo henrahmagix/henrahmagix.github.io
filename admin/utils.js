6000abb9bb8641a153d47bed125d2f51b052b922
Henry Blyth <blyth.henry@gmail.com>
2020-04-10 14:18:52 +0100

    add jsdoc types to utils.js

--- admin/utils.js
+++ admin/utils.js
@@ -1,19 +1,28 @@
+/** @param {string} s
+ * @returns {HTMLElement}
+*/
 export function createHTML(s) {
   const holder = document.createElement('div');
   holder.innerHTML = s;
-  return holder.children.item(0);
+  if (holder.childElementCount > 1) {
+    throw new Error('createHTML must receive only one root element');
+  }
+  return /** @type {HTMLElement} */ (holder.children.item(0));
 }
 
+/** @param {string} s */
 export function slugify(s) {
-  // Use GitHub-flavoured Markdown's title id setting.
-  return createHTML(window.markdownToHTML(`# ${s}`)).id;
+  return s.toLowerCase().replace(/ /g, '-');
 }
 
 export const yamlString = {
+  /** @param {string} s */
   toYaml(s) {
     // Quote to avoid yaml problems.
     return JSON.stringify(s);
   },
+  /** @param {string} s
+   * @returns {string} */
   toString(s) {
     // Unquote.
     try {
@@ -26,15 +35,17 @@ export const yamlString = {
 
 // https://stackoverflow.com/a/30106551/3150057
 export const base64 = {
+  /** @param {string} str */
   encode(str) {
     // first we use encodeURIComponent to get percent-encoded UTF-8,
     // then we convert the percent encodings into raw bytes which
     // can be fed into btoa.
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
       function toSolidBytes(match, p1) {
-        return String.fromCharCode('0x' + p1);
+        return String.fromCharCode(Number('0x' + p1));
       }));
   },
+  /** @param {string} str */
   decode(str) {
     // Going backwards: from bytestream, to percent-encoding, to original string.
     return decodeURIComponent(atob(str).split('').map(function (c) {
@@ -43,20 +54,36 @@ export const base64 = {
   },
 };
 
+/**
+ * @param {HTMLElement} el
+ * @param {boolean} visible
+ */
 export function show(el, visible) {
   if (visible == null) {
     visible = true;
   }
   return el.hidden = !visible;
 }
+/** @param {HTMLElement} el */
 export function showing(el) {
   return !el.hidden;
 }
 
+ /**
+  * @typedef {object} StateListener
+  * @property {string} [from]
+  * @property {string} [to]
+  * @property {() => void} fn
+  */
+
 export class State {
-  constructor(/*...states*/) {
-    this._states = arguments;
+  /**
+   * @param {...string} states
+   */
+  constructor(...states) {
+    this._states = states;
     this._state = this._states[0];
+    /** @type {StateListener[]} */
     this._listeners = [];
 
     if (!this._states || this._states.length < 2) {
@@ -64,45 +91,54 @@ export class State {
     }
   }
 
+  /**
+   * @param {string} s
+   * @returns {boolean}
+   */
   is(s) {
     return this._state === s;
   }
+  /** @param {string} to */
   moveTo(to) {
     const from = this._state;
     this._state = to;
     this._callChangeListeners(from, to);
   }
 
+  /**
+   * @param {string} from
+   * @param {string} to
+   */
   _callChangeListeners(from, to) {
     this._listeners.forEach(l => {
-      if (typeof l === 'function') {
-        l();
-        return;
-      }
+      const matchesFromTo = l.from === from && l.to === to;
+      const justFunction = l.from == null && l.to == null;
 
-      if (l.from === from && l.to === to) {
+      if (matchesFromTo || justFunction) {
         l.fn();
-        return;
       }
     });
   }
 
-  addChangeListener() {
-    let listener = arguments[0];
-    if (
-      arguments.length === 3
-      && Boolean(typeof arguments[0])
-      && Boolean(typeof arguments[1])
-      && typeof arguments[2] !== 'function'
-    ) {
-      listener = {
-        from: arguments[0],
-        to: arguments[1],
-        fn: arguments[2],
-      };
-    } else if (typeof listener !== 'function') {
-      throw new TypeError(`addChangeListener args must be just function, or from:string to:string callback:function, but was ${arguments}`);
+  /**
+   * @param {(() => void) | string} fnOrFrom
+   * @param {string | undefined} [to]
+   * @param {(() => void) | undefined} [fn]
+   */
+  addChangeListener(fnOrFrom, to, fn) {
+    if (typeof fnOrFrom === 'function') {
+      this._listeners.push({fn: fnOrFrom});
+      return;
     }
-    this._listeners.push(listener);
+    if (
+      typeof fnOrFrom === 'string'
+      && typeof to === 'string'
+      && typeof fn === 'function'
+    ) {
+      this._listeners.push({ from: fnOrFrom, to, fn });
+      return;
+    }
+
+    throw new TypeError(`addChangeListener args must be just function, or from:string to:string callback:function, but was ${arguments}`);
   }
 }
