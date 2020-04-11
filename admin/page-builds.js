eb061a48bac6b1d06ea6f56bc2bd6fe26345b0af
Henry Blyth <blyth.henry@gmail.com>
2020-04-11 17:05:58 +0100

    show build failing

--- admin/page-builds.js
+++ admin/page-builds.js
@@ -3,6 +3,7 @@ import { createHTML } from './utils.js';
 
 export class PageBuildStatus {
   get waitingEl() { return this.el.querySelector('.waiting'); }
+  get waitingText() { return this.waitingEl.querySelector('.text'); }
 
   constructor() {
     this.api = new Api();
@@ -10,7 +11,7 @@ export class PageBuildStatus {
     this.el = createHTML(`
       <div>
         <p class="waiting">
-          Build waiting
+          <span class="text">Build waiting</span>
           <button class="button-link" onclick="location.reload()">
             <i class="fas fa-sync-alt"></i>
           </button>
@@ -26,7 +27,12 @@ export class PageBuildStatus {
   async checkForCommit(commit) {
     const res = await this.api.makeRequest('/pages/builds')
     const latest = res[0];
+
     const buildWaiting = latest.status !== 'built' || latest.commit !== commit;
+    if (latest.status === 'error') {
+      this.waitingText.textContent = 'Build failing!';
+      this.waitingEl.classList.add('error');
+    }
 
     return buildWaiting;
   }
