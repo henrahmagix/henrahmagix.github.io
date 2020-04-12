3296da3bd3ce8364fd12061f9094a63ca81d13df
Henry Blyth <blyth.henry@gmail.com>
2020-04-12 09:48:08 +0100

    use custom element (web component) for list admin

--- admin/post-list-admin.js
+++ admin/post-list-admin.js
@@ -1,76 +1,85 @@
 import { Admin } from './admin.js';
 import { createHTML, show } from './utils.js';
 import { PostDraft } from './post-draft.js';
-import { addStyle } from './post-admin.js';
 
-/**
- * @param {object} opts
- * @param {HTMLElement} opts.contentElement
- */
-export function addPostListAdminView({
-  contentElement,
-}) {
-  addStyle('/admin/admin.css');
+const template = createHTML(`
+  <section id="admin-post-list">
+    <h2>Blog Admin</h2>
 
-  const adminInterface = createHTML(`
-    <section id="admin-post-list">
-      <h2>Blog Admin</h2>
+    <div class="side-by-side">
+      <ul class="drafts">
+        Drafts:
+        <li data-keep><a href="/admin/edit"><i class="icon fas fa-plus"></i>New</a></li>
+        <li><i class="icon fas fa-spinner fa-pulse"></i></li>
+      </ul>
 
-      <div class="side-by-side">
-        <ul class="drafts">
-          Drafts:
-          <li data-keep><a href="/admin/edit"><i class="icon fas fa-plus"></i>New</a></li>
-          <li><i class="icon fas fa-spinner fa-pulse"></i></li>
-        </ul>
+      <ul class="unsaved">
+        Unsaved changes:
+      </ul>
+    </div>
 
-        <ul class="unsaved">
-          Unsaved changes:
-        </ul>
-      </div>
+    <hr>
+  </section>
+`);
 
-      <hr>
-    </section>
-  `);
-  contentElement.before(adminInterface);
+/** @param {string} href */
+function linkStylesheet(href) {
+  const linkElem = document.createElement('link');
+  linkElem.setAttribute('rel', 'stylesheet');
+  linkElem.setAttribute('href', href);
+  return linkElem;
+}
 
-  const draftsList = adminInterface.querySelector('ul.drafts');
-  const unsavedList = adminInterface.querySelector('ul.unsaved');
+export class PostListAdminView extends HTMLElement {
+  constructor() {
+    super();
 
-  new Admin({
-    handleLogin: (loggedIn) => {
-      if (!loggedIn) {
-        return;
-      }
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(template.cloneNode(true));
 
-      show(adminInterface, true);
+    document.head.querySelectorAll('link[rel=stylesheet]').forEach(link => {
+      shadowRoot.appendChild(link.cloneNode());
+    });
+    shadowRoot.appendChild(linkStylesheet('/admin/admin.css'));
 
-      for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const keyParts = localStorage.key(i).match(/^gh_post_(.*)/)
-        const filepath = keyParts && keyParts[1];
-        if (!filepath || filepath === 'new') {
-          // This is accessible in the drafts list already.
-          continue;
+    const draftsList = this.shadowRoot.querySelector('ul.drafts');
+    const unsavedList = this.shadowRoot.querySelector('ul.unsaved');
+
+    new Admin({
+      handleLogin: (loggedIn) => {
+        if (!loggedIn) {
+          return;
         }
 
-        const params = new URLSearchParams();
-        params.set('filepath', filepath);
-        unsavedList.appendChild(createHTML(`<li><a href="/admin/edit?${params}">${filepath}</a></li>`));
-      }
+        show(this, true);
 
-      PostDraft.list().then(drafts => {
-        draftsList.querySelectorAll('li').forEach(li => {
-          if (li.dataset.hasOwnProperty('keep')) {
-            return;
+        for (let i = 0; i < localStorage.length; i++) {
+          const keyParts = localStorage.key(i).match(/^gh_post_(.*)/)
+          const filepath = keyParts && keyParts[1];
+          if (!filepath || filepath === 'new') {
+            // This is accessible in the drafts list already.
+            continue;
           }
-          li.remove();
+
+          const params = new URLSearchParams();
+          params.set('filepath', filepath);
+          unsavedList.appendChild(createHTML(`<li><a href="/admin/edit?${params}">${filepath}</a></li>`));
+        }
+
+        PostDraft.list().then(drafts => {
+          draftsList.querySelectorAll('li').forEach(li => {
+            if (li.dataset.hasOwnProperty('keep')) {
+              return;
+            }
+            li.remove();
+          });
+          drafts.forEach(d => {
+            const li = createHTML('<li></li>');
+            li.appendChild(d.el);
+            draftsList.appendChild(li);
+          });
         });
-        drafts.forEach(d => {
-          const li = createHTML('<li></li>');
-          li.appendChild(d.el);
-          draftsList.appendChild(li);
-        });
-      });
-    }
-  });
+      }
+    });
+  }
 }
