e12af9cdc1bb1964a51272d76f39ffaf7bfb6632
Henry Blyth <blyth.henry@gmail.com>
2020-04-17 23:50:45 +0100

    add jsodc typings to ipad cursor code

---
+++ _includes/ipad-cursor-trigger.js
@@ -0,0 +1,67 @@
+(function () {
+  var iPadCursorTrigger = /** @type {HTMLInputElement} */ (document.getElementById('{{include.checkbox_id}}'));
+  /** @type {HTMLElement[]} */
+  var iPadCursorAssets = [];
+
+  iPadCursorTrigger.addEventListener('change', function (event) {
+    var action = iPadCursorTrigger.checked ? 'on' : 'off';
+    window.gtag && window.gtag('event', action, {
+      event_category: 'ipad_cursor',
+      event_label: 'Features'
+    });
+
+    onCursorTriggerChange(event);
+  }, false);
+
+  var localStorage = window.localStorage || {
+    getItem: function (key) { return ''; },
+    setItem: function (key, val) { },
+  };
+
+  if (localStorage.getItem('hb_ipad_cursor') === '1') {
+    iPadCursorTrigger.checked = true;
+    if ('createEvent' in document) {
+      var evt = document.createEvent('HTMLEvents');
+      evt.initEvent('change', false, true);
+      iPadCursorTrigger.dispatchEvent(evt);
+    } else {
+      // @ts-ignore
+      iPadCursorTrigger.fireEvent('onchange');
+    }
+  }
+
+  /** @param {Event} event */
+  function onCursorTriggerChange(event) {
+    if (iPadCursorTrigger.checked) {
+      localStorage.setItem('hb_ipad_cursor', '1');
+      tryIpadCursor();
+    } else {
+      localStorage.setItem('hb_ipad_cursor', '0');
+      iPadCursorAssets.forEach(function (a) { a.remove(); });
+      window.iPadCursorDestroy();
+    }
+  }
+
+  function tryIpadCursor() {
+    var script = document.createElement('script');
+    script.src = '/js/ipad-cursor.js';
+    script.onerror = function () {
+      var msg = document.createElement('span');
+      msg.innerText = 'oh no? i dont know why something went wrong, soz :(';
+      msg.style.marginLeft = '0.25em';
+      msg.style.verticalAlign = 'middle';
+      iPadCursorTrigger.parentElement.appendChild(msg);
+    };
+
+    script.onload = function () {
+      var link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.type = 'text/css';
+      link.href = '/css/ipad-cursor.css';
+      document.head.appendChild(link);
+      iPadCursorAssets.push(link);
+    };
+    document.body.appendChild(script);
+    iPadCursorAssets.push(script);
+  }
+}());
--- js/ipad-cursor.js
+++ js/ipad-cursor.js
@@ -1,5 +1,6 @@
 (function(){
 
+/** @type {function[]} */
 var removerFunctions = [];
 
 var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
@@ -12,6 +13,7 @@ cursor.id = 'ipad-cursor';
 var styles = document.createElement('style');
 styles.innerText = '* { cursor: none!important; }';
 
+/** @type {DOMRect} */
 var boundPosition = null;
 
 runEventOnce(document, 'mousemove', start);
@@ -26,15 +28,16 @@ function start() {
   });
 }
 
+/** @type {number} */
 var fadeTimeout;
 addEventListener(document, 'mousemove', function (event) {
-  cursor.setAttribute('hidden', false);
-  clearTimeout(fadeTimeout);
+  cursor.setAttribute('hidden', 'false');
+  window.clearTimeout(fadeTimeout);
 
   positionCursorForMouseEvent(event);
 
-  fadeTimeout = setTimeout(function () {
-    cursor.setAttribute('hidden', true);
+  fadeTimeout = window.setTimeout(function () {
+    cursor.setAttribute('hidden', 'true');
   }, 3000);
 });
 
@@ -64,17 +67,24 @@ document.querySelectorAll('label').forEach(function (el) {
 });
 
 window.iPadCursorDestroy = function () {
-  removerFunctions.forEach(function (fn) { fn(); })
+  removerFunctions.forEach(function (fn) { fn(); });
 };
 
 var PAD = 10;
 
 var duration = 70;
+/** @type {number} */
 var lastTransition;
 
 /* funcs */
 
+/**
+ * @param {EventTarget} target
+ * @param {string} name
+ * @param {(event: MouseEvent) => void} fn
+ */
 function runEventOnce(target, name, fn) {
+  /** @type {(event: MouseEvent) => void} */
   var wrapped = function (event) {
     fn(event);
     target.removeEventListener(name, wrapped);
@@ -82,6 +92,11 @@ function runEventOnce(target, name, fn) {
   addEventListener(target, name, wrapped);
 }
 
+/**
+ * @param {EventTarget} target
+ * @param {string} name
+ * @param {(event: MouseEvent) => void} fn
+ */
 function addEventListener(target, name, fn) {
   target.addEventListener(name, fn);
   removerFunctions.push(function () {
@@ -89,6 +104,7 @@ function addEventListener(target, name, fn) {
   });
 }
 
+/** @param {Element} el */
 function bindCursor(el) {
   boundPosition = el.getBoundingClientRect();
   lastTransition = Date.now();
@@ -105,6 +121,12 @@ var ARC_BOTTOM_RIGHT = ARC_START+' -'+PAD+','+PAD;
 var ARC_BOTTOM_LEFT =  ARC_START+' -'+PAD+',-'+PAD;
 var ARC_TOP_LEFT =     ARC_START+' '+PAD+ ',-'+PAD;
 
+/**
+ * @param {number} moveX
+ * @param {number} moveY
+ * @param {number} w
+ * @param {number} h
+ */
 function drawRoundedRect(moveX, moveY, w, h) {
   // Move to top center of shape, draw half-width to top-right, clockwise around
   // to top-left, and z will connect back to the top center.
@@ -122,7 +144,14 @@ function drawRoundedRect(moveX, moveY, w, h) {
   );
 }
 
+/** @type {number} */
 var animating = null;
+/**
+ * @param {'in'|'out'} easeInOrOut
+ * @param {number[]} a
+ * @param {number[]} b
+ * @param {(...args: number[]) => void} fn
+ */
 function animate(easeInOrOut, a, b, fn) {
   var easeFn = easeInOut;
   if (easeInOrOut === 'in') {
@@ -160,7 +189,16 @@ function animate(easeInOrOut, a, b, fn) {
   animating = requestAnimationFrame(anim);
 }
 
+/** @type {{[key: string]: number}} */
 var animateBetweenCache = {};
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} timePosition
+ * @param {number} timeTotal
+ * @param {*} easeFn
+ * @returns
+ */
 function animateBetween(a, b, timePosition, timeTotal, easeFn) {
   var key = Array.prototype.join.call(arguments, '');
   if (animateBetweenCache.hasOwnProperty(key)) {
@@ -171,17 +209,22 @@ function animateBetween(a, b, timePosition, timeTotal, easeFn) {
 }
 
 // https://gist.github.com/gre/1650294
+/** @param {number} n */
 function easeIn(n) {
   return n*n*n; // cubic
 }
+/** @param {number} n */
 function easeOut(n) {
   return (--n)*n*n+1; // cubic
 }
+/** @param {number} n */
 function easeInOut(n) {
   return n<.5 ? 4*n*n*n : (n-1)*(2*n-2)*(2*n-2)+1; // cubic
 }
 
+/** @type {number[]} */
 var lastAnim = null;
+/** @param {MouseEvent} event */
 function positionCursorForMouseEvent(event) {
   // Mouse position by default.
   var x = event.x;
@@ -217,6 +260,11 @@ function positionCursorForMouseEvent(event) {
   });
 }
 
+/**
+ * @param {number} x
+ * @param {number} d
+ * @returns {number}
+ */
 function getElasticDistance(x, d) {
   // Use Apple's formula for rubber band scrolling:
   // b = (1.0 – (1.0 / ((x * c / d) + 1.0))) * d
